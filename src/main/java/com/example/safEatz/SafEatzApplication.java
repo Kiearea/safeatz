9df69566d55a95148c19488388fc0067120f764d
Kiearea <raleigh_f@yahoo.com>
2019-06-19 14:22:11 -0500

    Added gradle files to the safeatz folder

---
+++ src/main/java/com/example/safEatz/SafEatzApplication.java
@@ -0,0 +1,13 @@
+package com.example.safEatz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SafEatzApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SafEatzApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/safEatz/SafEatzApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.safEatz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SafEatzApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
