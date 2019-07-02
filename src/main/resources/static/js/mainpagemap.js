a8c3d6d7273ce4f3519d2d8804c3eb3a61579eb9
Kiearea <raleigh_f@yahoo.com>
2019-07-02 09:40:06 -0500

    Got modal to show up

--- src/main/resources/static/js/mainpagemap.js
+++ src/main/resources/static/js/mainpagemap.js
@@ -1,16 +1,44 @@
-//map
-var mymap = L.map('mapid').setView([37.0902, -95.7129], 5);
-
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox.streets',
-    accessToken: 'pk.eyJ1IjoiZW1tYXIxMTYiLCJhIjoiY2p4NmJta3BrMDF5djRhczJkeHJpa2ZrbSJ9.cXAK6dADsONnen5z6lHKCw'
-}).addTo(mymap);
-
-
 document.addEventListener('DOMContentLoaded', function() {
     console.log("Document ready!");
+
+    //map
+    var mymap = L.map('mapid').setView([37.0902, -95.7129], 5);
+
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+        maxZoom: 18,
+        id: 'mapbox.streets',
+        accessToken: 'pk.eyJ1IjoiZW1tYXIxMTYiLCJhIjoiY2p4NmJta3BrMDF5djRhczJkeHJpa2ZrbSJ9.cXAK6dADsONnen5z6lHKCw'
+    }).addTo(mymap);
+
+
+
+    var drawnItems = new L.FeatureGroup();
+    mymap.addLayer(drawnItems);
+
+    var drawControl = new L.Control.Draw({
+        edit: {
+            featureGroup: drawnItems
+        }
+    });
+
+
+    var drawControl = new L.Control.Draw({
+        edit: {
+            featureGroup: drawnItems
+        }
+    });
+
+    mymap.addControl(drawControl);
+
+    /** This is handled below.
+    mymap.on('draw:created', function (e) {
+       var type = e.layerType,
+           layer = e.layer;
+       drawnItems.addLayer(layer);
+    });
+    */
+
 
     // Fetch pins and plot them
     fetch("/api")
@@ -30,6 +58,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
            $("form input[name='lat']").val(latlng.lat);
            $("form input[name='lng']").val(latlng.lng);
+           $("#myModal").modal()
 
        }
 
@@ -37,25 +66,3 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
-
-var drawnItems = new L.FeatureGroup();
-mymap.addLayer(drawnItems);
-
-var drawControl = new L.Control.Draw({
-edit: {
-featureGroup: drawnItems
-}
-});
-
-
-var drawControl = new L.Control.Draw({
-    edit: {
-    featureGroup: drawnItems
-    }
-    });
-mymap.addControl(drawControl);
-     mymap.on('draw:created', function (e) {
-           var type = e.layerType,
-               layer = e.layer;
-           drawnItems.addLayer(layer);
-       });
